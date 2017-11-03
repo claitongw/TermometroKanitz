8dd37733164cf5d2c5e8d3aa3de2287a82641f9b
Claiton Watthier <cla.cgw@gmail.com>
2017-11-03 06:51:55 -0200

    dividir

--- js/calculaX.js
+++ js/calculaX.js
@@ -1,17 +1,29 @@
 
 function calcula(){
 
+
+
 	function calculaX1 () {
 		var LL = document.getElementById("LL").value;
-		LL = LL.replace(',','.')
+		LL = LL.toString().replace('.','');
+		LL = LL.toString().replace('.','');
+		LL = LL.toString().replace('.','');
+		LL = LL.replace(',','.');
 
 		var PL = document.getElementById("PL").value;
-		PL = PL.replace(',','.')
+		PL = PL.toString().replace('.','');
+		PL = PL.toString().replace('.','');
+		PL = PL.toString().replace('.','');
+		PL = PL.replace(',','.');
+
 
 		var x1 = LL/PL;
 
 		console.log(x1);
 	}
+
+
+
 
 	function calculaX2 (){
 
@@ -20,5 +32,3 @@ function calcula(){
 calculaX1();
 
 }
-
-
